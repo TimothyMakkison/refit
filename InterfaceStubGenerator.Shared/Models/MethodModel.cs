4489534d0bc59b6cac36b11011b86fa5a7193a16
Timothy Makkison <timmakkison@gmail.com>
2024-10-16 23:00:48 +0100

    feat: add `RefitBodyModel`

--- InterfaceStubGenerator.Shared/Models/MethodModel.cs
+++ InterfaceStubGenerator.Shared/Models/MethodModel.cs
@@ -16,3 +16,32 @@ internal enum ReturnTypeInfo : byte
     AsyncVoid,
     AsyncResult
 }
+
+internal record RefitBodyModel(
+    HttpMethod HttpMethod,
+    ImmutableEquatableArray<ParameterFragmentModel> PathFragments,
+    ImmutableEquatableArray<HeaderModel> Headers,
+    ImmutableEquatableArray<HeaderParameterModel> HeaderParameters,
+    string? HeaderCollectionParam,
+    ImmutableEquatableArray<AuthoriseModel> AuthoriseParameters,
+    ImmutableEquatableArray<PropertyModel> Properties,
+    ImmutableEquatableArray<QueryModel> QueryParameters,
+    BodyModel? BodyParameter,
+    UriFormat UriFormat
+);
+
+internal record struct HeaderModel(string Key, string Value);
+internal record struct HeaderParameterModel(string Parameter, string Header);
+internal record struct PropertyModel(string Parameter, string Key);
+internal record struct AuthoriseModel(string Parameter, string Scheme);
+internal record ParameterFragmentModel(string? Value, string? AccessExpression);
+internal record QueryModel(string Parameter, CollectionFormat CollectionFormat, string Delimiter, string? Prefix, string? Format);
+internal record BodyModel(string Parameter, bool Buffered, BodySerializationMethod SerializationMethod);
+
+
+internal enum BodyParameterType
+{
+    Content,
+    Stream,
+    String,
+}
